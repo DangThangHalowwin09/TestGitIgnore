a8b1610c74136b121217499bc57257533f77bcd5
DangThangHalowwin09 <107424970+DangThangHalowwin09@users.noreply.github.com>
2022-11-27 11:29:20 +0700

    Networking and Main Menu

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public int maxPlayers;
+
+    public static NetworkManager instance;
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+            gameObject.SetActive(false);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+    public void CreateRoom(string roomName)
+    {
+        //Create different rooms
+        RoomOptions Roptions = new RoomOptions();
+        // intial the number of players
+        Roptions.MaxPlayers = (byte)maxPlayers;
+
+        PhotonNetwork.CreateRoom(roomName, Roptions);
+    }
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+    [PunRPC]
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+    
+}
